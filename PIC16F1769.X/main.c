0c82ff3ac35a14267c9b8c8e58d41e632fe3e16b
Rafael Braga <rafael.braga@ecomp.ufsm.br>
2025-06-07 17:32:11 -0300

    Add files via upload

---
+++ PIC16F1769.X/main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: rafael
+ *
+ * Created on June 7, 2025, 4:44 PM
+ */
+
+
+#include <xc.h>
+#include "mcc_generated_files/system/system.h"
+#include "mcc_generated_files/pwm/pwm5.h"
+
+
+void main(void) {
+    SYSTEM_Initialize();              // Inicializa todos os periféricos gerados
+
+ 
+    while (1) {
+
+    }
+}
